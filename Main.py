5be128fcc04c2c180484a104d15db16ff418e04b
Lunar <lvaserveremail@gmail.com>
2023-05-09 10:02:59 -0700

    Update Main.py
    Adjusted some library usages, along with a GUI. This is still a framework, so some adjusting is still needed

--- Main.py
+++ Main.py
@@ -1,171 +1,91 @@
+import psutil
+import pyautogui
+import torch
 import cv2
 import numpy as np
-import win32gui
-import win32api
-import tensorflow as tf
-import random
+import tkinter as tk
+from PIL import ImageTk, Image
 
-# define the game window title
-GAME_WINDOW_TITLE = "Flash Game"
+# initialize PyTorch model
+model = torch.nn.Sequential(
+    torch.nn.Linear(8, 256),
+    torch.nn.ReLU(),
+    torch.nn.Linear(256, 512),
+    torch.nn.ReLU(),
+    torch.nn.Linear(512, 3),
+    torch.nn.Sigmoid()
+)
 
+# initialize OpenCV object detector
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# manually select the game window
-def select_game_window():
-    windows = []
-    win32gui.EnumWindows(lambda hwnd, windows:
-                         windows.append(hwnd) if win32gui.IsWindowVisible(hwnd) and len(
-                             win32gui.GetWindowText(hwnd)) > 0 else None, windows)
-    game_window = None
-    while game_window is None:
-        print("Select the window to play the game and press enter...")
-        input()
-        for hwnd in windows:
-            if GAME_WINDOW_TITLE in win32gui.GetWindowText(hwnd):
-                game_window = hwnd
-                break
-        if game_window is None:
-            print("No window found with title:", GAME_WINDOW_TITLE)
-    print("Window selected:", GAME_WINDOW_TITLE)
-    return game_window
+# initialize GUI
+root = tk.Tk()
+canvas = tk.Canvas(root, width=600, height=400)
+canvas.pack()
 
+# function to get screenshot of game
+def get_screenshot():
+    # get list of running processes
+    processes = []
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == 'game.exe':
+            processes.append(process)
+    
+    # get process with highest memory usage
+    process = max(processes, key=lambda p: p.memory_info().rss)
+    
+    # get screenshot of game window
+    window = pyautogui.getWindowsWithTitle(process.info['name'])[0]
+    x, y, width, height = window.left, window.top, window.width, window.height
+    screenshot = pyautogui.screenshot(region=(x, y, width, height))
+    return np.array(screenshot)
 
-# get the screenshot of the game window
-def get_screen(game_window):
-    left, top, right, bottom = win32gui.GetWindowRect(game_window)
-    width = right - left
-    height = bottom - top
-    screenshot = np.array(cv2.cvtColor(np.array(win32gui.GetWindowDC(game_window)), cv2.COLOR_BGR2RGB))
-    return screenshot, width, height
+# function to preprocess image for model input
+def preprocess(image):
+    # convert to grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # resize to 64x64
+    image = cv2.resize(image, (64, 64))
+    # convert to float32 and normalize to [0, 1]
+    image = image.astype(np.float32) / 255.0
+    # reshape to (1, 1, 64, 64)
+    image = np.reshape(image, (1, 1, 64, 64))
+    return torch.from_numpy(image)
 
+# function to get model output and execute action
+def execute_action(image):
+    # preprocess image
+    input = preprocess(image)
+    # get model output
+    output = model(input)
+    # get action from output
+    action = torch.argmax(output).item()
+    # execute action
+    if action == 0:
+        # move mouse to center of screen
+        pyautogui.moveTo(pyautogui.size().width / 2, pyautogui.size().height / 2)
+    elif action == 1:
+        # left click
+        pyautogui.click()
+    elif action == 2:
+        # press up arrow key
+        pyautogui.press('up')
 
-# define the deep-Q neural network
-class DeepQNetwork(tf.keras.Model):
-    def __init__(self, state_size, action_size):
-        super(DeepQNetwork, self).__init__()
-        self.fc1 = tf.keras.layers.Dense(128, activation='relu', input_shape=state_size)
-        self.fc2 = tf.keras.layers.Dense(128, activation='relu')
-        self.fc3 = tf.keras.layers.Dense(action_size, activation='linear')
-
-    def call(self, state):
-        x = self.fc1(state)
-        x = self.fc2(x)
-        q_values = self.fc3(x)
-        return q_values
-
-
-# define the replay buffer
-class ReplayBuffer:
-    def __init__(self, buffer_size):
-        self.buffer_size = buffer_size
-        self.buffer = []
-
-    def add(self, experience):
-        if len(self.buffer) + len(experience) >= self.buffer_size:
-            self.buffer[0:(len(experience) + len(self.buffer)) - self.buffer_size] = []
-        self.buffer.extend(experience)
-
-    def sample(self, batch_size):
-        return np.reshape(np.array(random.sample(self.buffer, batch_size)), [batch_size, 5])
-
-
-# define the deep-Q learning algorithm
-class DeepQLearning:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
-        self.discount_factor = 0.99
-        self.batch_size = 64
-        self.memory_size = 1000000
-        self.replay_buffer = ReplayBuffer(self.memory_size)
-        self.model = DeepQNetwork(self.state_size, self.action_size)
-        self.target_model = DeepQNetwork(self.state_size, self.action_size)
-        self.target_model.set_weights(self.model.get_weights())
-        self.optimizer = tf.keras.optimizers.Adam(lr=self.learning_rate)
-
-    def get_action(self, state):
-        if np.random.rand() <= self.epsilon:
-            action = np.random.randint(self.action_size)
-            return action
-
-
-def train(self):
-    if len(self.replay_buffer.buffer) < self.batch_size:
-        return
-    mini_batch = self.replay_buffer.sample(self.batch_size)
-    states = np.array([transition[0] for transition in mini_batch])
-    actions = np.array([transition[1] for transition in mini_batch])
-    rewards = np.array([transition[2] for transition in mini_batch])
-    next_states = np.array([transition[3] for transition in mini_batch])
-    dones = np.array([transition[4] for transition in mini_batch])
-
-    target = self.model.predict(states)
-    target_next = self.target_model.predict(next_states)
-
-    for i in range(self.batch_size):
-        target[i][actions[i]] = rewards[i] + self.discount_factor * np.amax(target_next[i]) * (1 - dones[i])
-
-    self.model.fit(states, target, batch_size=self.batch_size, epochs=1, verbose=0)
-
-
-def update_target_model(self):
-    self.target_model.set_weights(self.model.get_weights())
-
-
-def play_game(self):
-    game_window = select_game_window()
-    state, width, height = get_screen(game_window)
-    state_size = (width, height, 3)
-    action_size = 4
-
-    self.model = DeepQNetwork(state_size, action_size)
-    self.target_model = DeepQNetwork(state_size, action_size)
-    self.target_model.set_weights(self.model.get_weights())
-
-    total_episodes = 1000
-    steps = 0
-    for episode in range(total_episodes):
-        done = False
-        score = 0
-        while not done:
-            action = self.get_action(state)
-            reward = 0
-            if action == 0:
-                # Perform left-click action
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-                reward = 1
-            elif action == 1:
-                # Perform right-click action
-                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
-                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
-                reward = 1
-            elif action == 2:
-                # Perform move left action
-                win32api.keybd_event(win32con.VK_LEFT, 0, 0, 0)
-                win32api.keybd_event(win32con.VK_LEFT, 0, win32con.KEYEVENTF_KEYUP, 0)
-            elif action == 3:
-                # Perform move right action
-                win32api.keybd_event(win32con.VK_RIGHT, 0, 0, 0)
-                win32api.keybd_event(win32con.VK_RIGHT, 0, win32con.KEYEVENTF_KEYUP, 0)
-
-            next_state, _, _ = get_screen(game_window)
-            self.replay_buffer.add([(state, action, reward, next_state, done)])
-            state = next_state
-
-            steps += 1
-            if steps % 4 == 0:
-                self.train()
-
-            if steps % 100 == 0:
-                self.update_target_model()
-
-            score += reward
-
-        print("Episode:", episode, " Score:", score)
-
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+# function to update GUI
+def update_gui():
+    # get screenshot
+    screenshot = get_screenshot()
+    # detect objects in screenshot
+    objects = detector.detectMultiScale(screenshot, scaleFactor=1.1, minNeighbors=5)
+    # draw boxes around detected objects
+    for (x, y, w, h) in objects:
+        cv2.rectangle(screenshot, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    # convert screenshot to ImageTk format
+    image = Image.fromarray(screenshot)
+    image_tk = ImageTk.PhotoImage(image)
+    # update canvas
+    canvas.create_image(0, 0, anchor='nw', image=image_tk)
+    # schedule next GUI update
+    root.after(100, update_gui)
+root.mainloop()
