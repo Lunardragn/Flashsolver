cfdf292a2f8e037ecca80f7ea215996fad17767a
Lunar <lvaserveremail@gmail.com>
2023-05-09 10:18:35 -0700

    Update Main.py
    Better framework

--- Main.py
+++ Main.py
@@ -1,151 +1,93 @@
-import pyautogui
 import psutil
-import time
-import cv2
+import pyautogui
 import torch
-import numpy as np
+import cv2
 import tkinter as tk
 
-class GameAgent:
-    def __init__(self, game_name):
-        self.game_name = game_name
-        self.game_process = None
-        self.current_state = None
-        self.last_score = None
-        self.last_action = None
-        self.last_state = None
-        self.last_reward = None
-        self.reward_sum = 0
-        self.learning_rate = 0.001
-        self.discount_factor = 0.99
-        self.memory = []
-        self.batch_size = 32
-        self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.model = self.create_model()
-        self.actions = ['up', 'down', 'left', 'right', 'click']
+# Function to find game process using psutil
+def find_process(process_name):
+    for process in psutil.process_iter():
+        if process.name().lower() == process_name.lower():
+            return process.pid
+    return None
 
-    def create_model(self):
-        model = torch.nn.Sequential(
-            torch.nn.Linear(6400, 512),
-            torch.nn.ReLU(),
-            torch.nn.Linear(512, 5)
-        )
-        model.load_state_dict(torch.load('model.pt'))
-        model.eval()
-        return model
-
-    def update_epsilon(self):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def get_game_window(self):
-        for proc in psutil.process_iter(['pid', 'name', 'memory_info']):
-            if self.game_name.lower() in proc.info['name'].lower():
-                self.game_process = proc
-                return proc.pid
-
-    def get_state(self):
-        hwnd = self.get_game_window()
-        if hwnd is None:
-            return None
-
-        bbox = (0, 0, 800, 600)
-        img = pyautogui.screenshot(region=bbox)
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-        img = cv2.resize(img, (80, 60))
-        return img.flatten()
-
-    def get_action(self, state):
-        if np.random.rand() <= self.epsilon:
-            action = np.random.choice(self.actions)
-        else:
-            state_tensor = torch.tensor(state, dtype=torch.float32)
-            q_values = self.model(state_tensor)
-            action_idx = torch.argmax(q_values).item()
-            action = self.actions[action_idx]
-
-        self.last_action = action
-        return action
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-        self.reward_sum += reward
-        if done:
-            self.last_reward = self.reward_sum
-            self.reward_sum = 0
-
-        if len(self.memory) > 10000:
-            self.memory.pop(0)
-
-    def train(self):
-        if len(self.memory) < self.batch_size:
-            return
-
-        batch = np.random.choice(len(self.memory), self.batch_size, replace=False)
-        states = []
-        targets = []
-        for i in batch:
-            state, action, reward, next_state, done = self.memory[i]
-            state_tensor = torch.tensor(state, dtype=torch.float32)
-            next_state_tensor = torch.tensor(next_state, dtype=torch.float32)
-            q_values = self.model(state_tensor)
-            next_q_values = self.model(next_state_tensor)
-            if done:
-                target = reward
-            else:
-                target = reward + self.discount_factor * torch.max(next_q_values)
-                        q_values[action] = target
-        states.append(state)
-        targets.append(q_values)
-
-    states_tensor = torch.tensor(states, dtype=torch.float32)
-    targets_tensor = torch.stack(targets)
-    optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-    optimizer.zero_grad()
-    loss = torch.nn.MSELoss()(self.model(states_tensor), targets_tensor)
-    loss.backward()
-    optimizer.step()
-
-def play(self):
+# Function to create the GUI
+def create_gui():
     root = tk.Tk()
-    root.title("Game Agent")
-    canvas = tk.Canvas(root, width=800, height=600)
-    canvas.pack()
-    self.gui_loop(root, canvas)
+    root.title("Deep-Q Neural Network Game Player")
+
+    # Create label to show status
+    status_label = tk.Label(root, text="Status: Not running")
+    status_label.pack()
+
+    # Create label to show object recognition
+    object_label = tk.Label(root, text="Object recognition: ")
+    object_label.pack()
+
+    # Create label to show current action
+    action_label = tk.Label(root, text="Current action: ")
+    action_label.pack()
+
+    # Create button to show OpenCV what to look for
+    learn_button = tk.Button(root, text="Learn Object", command=learn_object)
+    learn_button.pack()
+
+    # Create button to select program to play
+    select_button = tk.Button(root, text="Select Program", command=select_program)
+    select_button.pack()
+
     root.mainloop()
 
-def gui_loop(self, root, canvas):
+# Function to teach the neural network using deep-Q learning
+def deep_q_learning():
+    # Load game
+    game_pid = find_process("game.exe")
+    game_window = pyautogui.getWindowsWithTitle("Game Window")[0]
+    game_window.activate()
+
+    # Initialize neural network using pytorch
+    net = torch.nn.Sequential(
+        torch.nn.Linear(3, 4),
+        torch.nn.ReLU(),
+        torch.nn.Linear(4, 3)
+    )
+
+    # Loop to play game and teach the neural network
     while True:
-        state = self.get_state()
-        if state is not None:
-            action = self.get_action(state)
-            self.execute_action(action)
+        # Take screenshot and use OpenCV to recognize objects
+        screenshot = pyautogui.screenshot()
+        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+        # object_recognition = opencv_function(screenshot)
 
-        if self.last_score is not None:
-            canvas.create_text(20, 20, anchor=tk.NW, text="Score: {}".format(self.last_score), fill='white')
+        # Use neural network to determine action
+        action = net(screenshot)
+        # execute_action(action)
 
-        canvas.update()
+        # Check for positive reinforcement (progress to next level)
+        if check_progression():
+            # reward_network(action)
+            break
 
-        time.sleep(0.1)
+    # Save trained neural network
+    torch.save(net.state_dict(), "game_network.pth")
 
-def execute_action(self, action):
-    if action == 'up':
-        pyautogui.press('up')
-    elif action == 'down':
-        pyautogui.press('down')
-    elif action == 'left':
-        pyautogui.press('left')
-    elif action == 'right':
-        pyautogui.press('right')
-    elif action == 'click':
-        pyautogui.click()
+# Function to manually select the program to play
+def select_program():
+    # Use tkinter file dialog to select program
+    file_path = tk.filedialog.askopenfilename(initialdir="/", title="Select Program", filetypes=(("Exe files", "*.exe"), ("All files", "*.*")))
+    # launch_game(file_path)
 
-def save_model(self):
-    torch.save(self.model.state_dict(), 'model.pt')
-   
-if name == 'main':
-game_agent = GameAgent('your_game_name')
-game_agent.play()
-           
+# Function to teach OpenCV what to look for
+def learn_object():
+    # Use tkinter file dialog to select object image
+    file_path = tk.filedialog.askopenfilename(initialdir="/", title="Select Object Image", filetypes=(("Image files", "*.png;*.jpg"), ("All files", "*.*")))
+    # object_image = opencv_function(file_path)
+    # save_object_image(object_image)
+
+# Main function to start the program
+def main():
+    create_gui()
+    deep_q_learning()
+
+if __name__ == "__main__":
+    main()
